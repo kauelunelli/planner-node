43e0ccd08701ce2b43c0fad999120957e7884246
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-05 00:42:18 -0300

    feat: Configure Jest for TypeScript unit testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+};
