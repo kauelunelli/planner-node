8a908758e25500ce28785c1f98a9b1d14b364069
Kaue Lunelli <lunellikaue@gmail.com>
2026-04-19 23:37:59 -0300

    feat: implement ClientError class with custom error code and message

--- src/errors/client-error.ts
+++ src/errors/client-error.ts
@@ -1,1 +1,8 @@
-export class ClientError extends Error { }
+export class ClientError extends Error {
+	code: string;
+
+	constructor(message: string, code = 'BUSINESS_RULE_VIOLATION') {
+		super(message);
+		this.code = code;
+	}
+}
